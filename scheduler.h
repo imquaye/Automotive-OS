58e31fafec4bf2073d6b959d74517c10c2bee098
k-9 <siripifrancis21@gmail.com>
2026-01-23 21:44:42 +0000

    Add comments to Task struct fields in scheduler.h
    Added comments to clarify the purpose of the 'period', 'deadline', and 'deadline_missed' fields in the Task struct.

--- scheduler.h
+++ scheduler.h
@@ -1,14 +1,19 @@
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
 
+#include <time.h>
+
 typedef struct {
     char name[30];
-    int period;
+    int period;           // Task period in ms
     int priority;
+    int deadline;         // Deadline in ms (typically equal to period)
+    int deadline_missed;  // Flag to track if deadline was missed
     void (*task_function)();
 } Task;
 
 void add_task(Task task);
 void run_scheduler();
+int get_total_deadline_misses();
 
 #endif
